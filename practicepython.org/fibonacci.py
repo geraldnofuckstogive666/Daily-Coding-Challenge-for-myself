58987fab73d2e7ab5a478d6e9744f1e6e0334613
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-02-09 15:48:29 +0000

    Day 10

---
+++ practicepython.org/fibonacci.py
@@ -0,0 +1,34 @@
+#https://www.practicepython.org/exercise/2014/04/30/13-fibonacci.html
+
+
+"""
+Write a program that asks the user how many Fibonnaci numbers to generate and 
+then generates them. Take this opportunity to think about how you can use functions. 
+Make sure to ask the user to enter the number of numbers in the sequence to generate.
+(Hint: The Fibonnaci seqence is a sequence of numbers where the next number in the sequence
+ is the sum of the previous two numbers in the sequence. The sequence looks like this: 
+ 1, 1, 2, 3, 5, 8, 13, …)
+
+
+"""
+import sys
+
+def fibonacci(num):
+    sequence = []
+    current_sequence = len(sequence)
+    a1 = 0
+    a2 = 1
+    if current_sequence == num:
+        return sequence
+
+    if num == 1:
+        pass
+
+
+    
+
+
+
+
+
+
