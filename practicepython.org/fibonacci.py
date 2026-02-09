3e2599137a7a33aeb749adfb81190b55185f4c9a
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-02-09 16:52:03 +0000

    Day 10

--- practicepython.org/fibonacci.py
+++ practicepython.org/fibonacci.py
@@ -8,24 +8,35 @@ Make sure to ask the user to enter the number of numbers in the sequence to gene
 (Hint: The Fibonnaci seqence is a sequence of numbers where the next number in the sequence
  is the sum of the previous two numbers in the sequence. The sequence looks like this: 
  1, 1, 2, 3, 5, 8, 13, …)
+ 
+ so the sequence we are using starts with 1, 1 and not 0, 1 ... then folowed by fib sequence
+
 
 
 """
+from functools import lru_cache
 import sys
 
+@lru_cache(maxsize=None)
 def fibonacci(num):
-    sequence = []
-    current_sequence = len(sequence)
-    a1 = 0
-    a2 = 1
-    if current_sequence == num:
-        return sequence
-
-    if num == 1:
-        pass
+    if  num in (1, 2):
+        return 1
+    return fibonacci(num - 1) + fibonacci(num - 2)
 
 
-    
+def fibonacci_sequential(num):
+    return [fibonacci(n) for n in range(1, num + 1)]
+
+
+try:
+    user_input = int(input("Fibonacci numbers to generate: "))
+    if user_input < 1:
+        sys.exit("Invalid input.")
+except ValueError:
+    sys.exit("Invalid input.")
+
+print(*fibonacci_sequential(user_input))
+
 
 
 
