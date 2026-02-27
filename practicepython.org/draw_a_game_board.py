4493b7105e34a6d64d4ed6dc60ef35903bdc3f7a
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-02-27 13:45:09 +0800

    Day 20

--- practicepython.org/draw_a_game_board.py
+++ practicepython.org/draw_a_game_board.py
@@ -1,7 +1,27 @@
 #https://www.practicepython.org/exercise/2014/12/27/24-draw-a-game-board.html
 
 
-"""
-I've run out of free codespaces. Will get back ASAP.
 
-"""
+def print_grid(row=3, column=3, cell_char=' '):
+    separator = ("+" + "---") * column + "+"
+    print(separator)
+    for _ in range(row):
+        rows = ("|" + f" {cell_char} ") * column + "|"
+        print(rows)
+        print(separator)
+
+def main():
+    print("      ---Draw a Game Board---\n")
+    try:
+        x = int(input("How many rows: ").strip())
+        y = int(input("How many columns: ").strip())
+        
+    except ValueError:
+        print("Invalid input. Must be a number.")
+
+    print_grid(x, y)
+    
+  
+
+if __name__ == "__main__":
+    main()
