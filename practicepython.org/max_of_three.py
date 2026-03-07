f25b819ec2a5bdc8ca513548e233811139bdeb2d
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-07 22:43:52 +0800

    Day 24

--- practicepython.org/max_of_three.py
+++ practicepython.org/max_of_three.py
@@ -1,3 +1,21 @@
 #https://www.practicepython.org/exercise/2016/03/27/28-max-of-three.html
 
 
+def max_of_three(a, b, c):
+    largest = a if a > b else b
+    return largest if largest > c else c
+    
+def main():
+    #test the max_of_three function
+    a = 10
+    b = 11
+    c = 7
+    
+    largest = max_of_three(a, b, c)
+    
+    print(largest)
+    
+if __name__ == "__main__":
+    main()
+
+
