f302b3b6ba40a810ab15225927a44a9b5ef2edda
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-17 15:57:19 +0000

    Day 26

--- practicepython.org/pick_word.py
+++ practicepython.org/pick_word.py
@@ -3,25 +3,30 @@
 
 import random
 
+FILE_NAME = 'sowpods_dictionary.txt'
 
 def fetch_words(file_name: str) -> list[str]:
     try:
-        with open(file_name, 'r') as open_file:
-            return [line.strip() for line in open_file]
+        with open(file_name, 'r') as file:
+            return [line.strip() for line in file]
     except FileNotFoundError:
         print(f"Error: {file_name} not found.")
         return []
 
 def pick_word(words: list[str])-> str:  #random word
     if not words:
-        return "No words available."
+        return None
     return random.choice(words)
 
 
 def main():
-    FILE_NAME = 'sowpods_dictionary.txt'
     sowpods = fetch_words(FILE_NAME) 
-    print(pick_word(sowpods))
+    word = pick_word(sowpods)
+
+    if word is None:
+        print("No words available.")
+    else:
+        print(word)
 
 
 if __name__ == "__main__":
