51f331c06bdcbc294b1ba9d0237064a6165318e5
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-03-19 11:49:25 +0000

    Day 28

--- practicepython.org/hangman.py
+++ practicepython.org/hangman.py
@@ -1,2 +1,12 @@
-#https://www.practicepython.org/exercise/2017/01/10/32-hangman.html
+#part 1 - https://www.practicepython.org/exercise/2016/09/24/30-pick-word.html
+#github: https://github.com/geraldnofuckstogive666/Daily-Coding-Challenge-for-myself/blob/main/practicepython.org/pick_word.py
 
+#part 2 - https://www.practicepython.org/exercise/2017/01/02/31-guess-letters.html
+#github: https://github.com/geraldnofuckstogive666/Daily-Coding-Challenge-for-myself/blob/main/practicepython.org/guess_letters.py
+
+#part 3 (this challenge) - https://www.practicepython.org/exercise/2017/01/10/32-hangman.html
+
+
+
+def main():
+    ...
